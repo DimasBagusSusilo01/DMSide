58956c267e20aef379e480dc3fd6fb3e6820ef58
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-08-13 18:44:04 +0700

    nambah tangan

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ async function createHandTracker() {
 
             runningMode: "VIDEO",
 
-            numHands: 1,
+            numHands: 2,
 
             minHandDetectionConfidence: 0.5,
             minHandPresenceConfidence: 0.5,
