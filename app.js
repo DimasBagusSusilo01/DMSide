441ca31ffb5d664b951d14c8dda51207edf7850a
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-08-17 14:33:41 +0700

    akses kamera

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const webcam = document.getElementById("webcam");
 const kanvas = document.getElementById("kanvas");
+
+async function startCamera() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video = true,
+        audio = false
+    });
+    webcam.srcObject = stream;
+}
+startCamera()
