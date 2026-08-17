54236acca5e67b2859e93fd4a5cdd54b407077f3
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-08-17 14:35:50 +0700

    ubah lagi

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const kanvas = document.getElementById("kanvas");
 
 async function startCamera() {
     const stream = await navigator.mediaDevices.getUserMedia({
-        video = true,
-        audio = false
+        video : true,
+        audio : false
     });
     webcam.srcObject = stream;
 }
